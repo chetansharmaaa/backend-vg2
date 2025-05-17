3c229cfbe9a22a2e21b1aae7d63ba2b9a46f2352
chetansharrrmaa <chetansharrrmaa@gmail.com>
2025-05-17 16:53:07 +0530

    init 2 docker add

--- src/UserManagementService/Program.cs
+++ src/UserManagementService/Program.cs
@@ -93,7 +93,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
 
 app.UseCors("AllowSpecificOrigin");
 
